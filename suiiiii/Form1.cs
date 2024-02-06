8da7e471f3fc9040fdca1e1e944670f3c802f550
BeardRubyBlue <sander.sisoev@gmail.com>
2024-02-06 16:04:00 +0300

    xnxfgnfg

---
+++ suiiiii/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace suiiiii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int seconds))
+            {
+                int minutes = seconds / 60;
+                MessageBox.Show($"Количество полных минут: {minutes}");
+            }
+            else
+            {
+                MessageBox.Show("Введите целое число секунд.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
